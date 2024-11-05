90e2052cf0abbee2f1dc615837ea1345ed28558d
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2024-11-05 08:40:14 +0100

    constantes.php et twig.php ont été développés

---
+++ config/constantes.php
@@ -0,0 +1,16 @@
+<?php
+
+// Connexion à la base de données (logins wamp)
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'timeharmony_bd');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// Spécifique aux bases de donnees sur lakartxela
+define('PREFIXE_TABLE', 'timeharmony_');
+
+// Constante de la partie vue
+define('WEBSITE_TITLE', 'TimeHarmony');
+define('WEBSITE_VERSION', '0.1');
+define('WEBSITE_DESCRIPTION', 'Une application web qui permet de trouver des créneaux de rendez-vous facilement');
+define('WEBSITE_LANGUAGE', 'fr');
