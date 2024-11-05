0d20f9625c42d624f08b8f964fa631fbd54f9bba
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2024-11-05 08:20:25 +0100

    continuation de l'aborescence

---
+++ include.php
@@ -0,0 +1,18 @@
+<?php
+
+//Ajout de l'autoload de composer
+require_once 'vendor/autoload.php';
+
+//Ajout du fichier constantes qui permet de configurer le site
+require_once 'config/constantes.php';
+
+//Ajout du code pour initialiser twig
+require_once 'config/twig.php';
+
+//Ajout du modèle qui gère la connexion mysql
+require_once 'modeles/bd.class.php';
+
+//Ajout des contrôleurs
+
+//Ajout des modèles
+
