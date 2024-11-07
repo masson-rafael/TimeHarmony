51d17f33e81fcd4d513f4ab16a4420da8389d9b4
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2024-11-07 10:27:02 +0100

    creation du controlleur, de utilisateur.dao, changement des liens vers la page non connecté par défaut, affichage de la navbar en dur (du a l'absence de bd), vue inscription créée

---
+++ controllers/controller_utilisateur.class.php
@@ -0,0 +1,23 @@
+<?php
+
+use ICal\ICal;
+
+class ControllerUtilisateur extends Controller
+{
+    public function __construct(\Twig\Environment $twig, \Twig\Loader\FilesystemLoader $loader)
+    {
+        parent::__construct($twig, $loader);
+    }
+
+    function connexion() {}
+
+    function inscription()
+    {
+
+
+
+        //Génération de la vue
+        $template = $this->getTwig()->load('inscription.html.twig');
+        echo $template->render();
+    }
+}
---
+++ modeles/utilisateur.dao.php
@@ -0,0 +1,40 @@
+<?php
+
+class UtilisateurDao{
+    private ?PDO $pdo;
+
+    public function __construct(?PDO $pdo=null){
+        $this->pdo = $pdo;
+    }
+
+    public function getPdo(): ?PDO
+    {
+        return $this->pdo;
+    }
+
+
+    public function setPdo($pdo): void
+    {
+        $this->pdo = $pdo;
+    }
+
+
+    public function find(?int $id): ?Utilisateur
+    {
+        $sql="SELECT * FROM ".PREFIXE_TABLE."utilisateur WHERE id= :id";
+        $pdoStatement = $this->pdo->prepare($sql);
+        $pdoStatement->execute(array("id"=>$id));
+        $pdoStatement->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Utilisateur');
+        $utilisateur = $pdoStatement->fetch();
+        return $utilisateur;
+    }
+
+    public function findAll(){
+        $sql="SELECT * FROM ".PREFIXE_TABLE."utilisateur";
+        $pdoStatement = $this->pdo->prepare($sql);
+        $pdoStatement->execute();
+        $pdoStatement->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Utilisateur');
+        $utilisateur = $pdoStatement->fetchAll();
+        return $utilisateur;
+    }
+}
