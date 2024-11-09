e8ed41e29bbebc353e3b352033ff3f19840de28c
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-11-09 23:16:26 +0100

    Ajout l'utilisateur ssi 8 <= mdp <= 12 et lettre min maj, nombre et caractere special

--- controllers/controller_utilisateur.class.php
+++ controllers/controller_utilisateur.class.php
@@ -23,8 +23,9 @@ class ControllerUtilisateur extends Controller
         if (isset($_POST['email']) && isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['pwd']) && isset($_POST['pwdConfirme'])) {
             $manager = new UtilisateurDao($pdo); //Lien avec PDO
             $utilisateurExiste = $manager->findMail($_POST['email']); //Appel fonction et stocke bool pour savoir si utilisateur existe deja avec email
+            $mdpContientBonsCaracteres = preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)[a-zA-Z\d\W]{8,16}$/', $_POST['pwd']);
 
-            if (!$utilisateurExiste && $_POST['pwd'] == $_POST['pwdConfirme']) {
+            if (!$utilisateurExiste && $_POST['pwd'] == $_POST['pwdConfirme'] && $mdpContientBonsCaracteres) {
                 $nouvelUtilisateur = new Utilisateur();
                 $nouvelUtilisateur->setEmail($_POST['email']);
                 $nouvelUtilisateur->setNom($_POST['nom']);
@@ -35,9 +36,11 @@ class ControllerUtilisateur extends Controller
                 $manager->ajouterUtilisateur($nouvelUtilisateur); //Appel du script pour ajouter utilisateur dans bd
                 
                 $this->genererVue($_POST['email'], $utilisateurExiste, "INSCRIPTION REUSSIE");
-            } else if (!$utilisateurExiste) {
+            } else if (!$utilisateurExiste && $_POST['pwd'] != $_POST['pwdConfirme']) {
                 $this->genererVue($_POST['email'], $utilisateurExiste, "MOTS DE PASSE NON IDENTIQUES");
-            } else {
+            } else if (!$utilisateurExiste) {
+                $this->genererVue($_POST['email'], $utilisateurExiste, "MOTS DE PASSE NE SUIT PAS LA REGLE");
+            }else {
                 $this->genererVue($_POST['email'], $utilisateurExiste, "UTILISATEUR EXISTE DEJA");
             }
         }
