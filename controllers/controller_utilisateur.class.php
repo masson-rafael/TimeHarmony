155c6136543284078af6adb2ccc58fdcd796819a
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-11-10 16:31:20 +0100

    Ajout d'une verification si email est considere valide

--- controllers/controller_utilisateur.class.php
+++ controllers/controller_utilisateur.class.php
@@ -29,13 +29,13 @@ class ControllerUtilisateur extends Controller
 
     function premiereInscription() {
         //Création d'une fonction appellee 1 seule fois pour ne pas avoir de double twig
-        $this->genererVueVide();
+        $this->genererVueVide('inscription');
         $this->inscription();
     }
 
     function premiereConnexion() {
         //Création d'une fonction appellee 1 seule fois pour ne pas avoir de double twig
-        $this->genererVueVideConnexion();
+        $this->genererVueVide('connexion');
         $this->connexion();
     }
 
@@ -49,18 +49,20 @@ class ControllerUtilisateur extends Controller
             $utilisateurExiste = $manager->findMail($_POST['email']); //Appel fonction et stocke bool pour savoir si utilisateur existe deja avec email
             $mdpContientBonsCaracteres = preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)[a-zA-Z\d\W]{8,16}$/', $_POST['pwd']);
 
-            if (!$utilisateurExiste && $_POST['pwd'] == $_POST['pwdConfirme'] && $mdpContientBonsCaracteres) {
+            if (!$utilisateurExiste && $_POST['pwd'] == $_POST['pwdConfirme'] && $mdpContientBonsCaracteres && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
                 $nouvelUtilisateur = Utilisateur::createAvecParam(null, $_POST['nom'], $_POST['prenom'], $_POST['email'], $_POST['pwd'], "photoProfil.jpg", false); //Création d'un nouvel utilisateur (instance)
                 $manager->ajouterUtilisateur($nouvelUtilisateur); //Appel du script pour ajouter utilisateur dans bd
                 $this->genererVue($_POST['email'], $utilisateurExiste, "INSCRIPTION REUSSIE");
             } else if (!$utilisateurExiste && $_POST['pwd'] != $_POST['pwdConfirme']) {
                 $this->genererVue($_POST['email'], $utilisateurExiste, "MOTS DE PASSE NON IDENTIQUES");
-            } else if (!$utilisateurExiste) {
+            } else if (!$utilisateurExiste && !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+                $this->genererVue($_POST['email'], $utilisateurExiste, "ADRESSE MAIL NON VALIDE");
+            }else if (!$utilisateurExiste) {
                 $this->genererVue($_POST['email'], $utilisateurExiste, "MOTS DE PASSE NE SUIT PAS LA REGLE");
-            }else {
+            } else {
                 $this->genererVue($_POST['email'], $utilisateurExiste, "UTILISATEUR EXISTE DEJA");
             }
-        }
+        } 
     }
 
     public function genererVue(?string $mail, ?bool $existe, ?string $message) {
@@ -77,17 +79,9 @@ class ControllerUtilisateur extends Controller
 
     //////////////////////////////////////////////////////////////// OPTIMISER CETTE MERDE
 
-    public function genererVueVide() {
+    public function genererVueVide(?string $page) {
         //Génération de la vue
-        $template = $this->getTwig()->load('inscription.html.twig');
-        echo $template->render(
-            array()
-        );
-    }
-
-    public function genererVueVideConnexion() {
-        //Génération de la vue
-        $template = $this->getTwig()->load('connexion.html.twig');
+        $template = $this->getTwig()->load($page . '.html.twig');
         echo $template->render(
             array()
         );
