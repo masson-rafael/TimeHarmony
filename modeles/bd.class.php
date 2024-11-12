2fc8171bdc56fb59942d1edfe6b08acf7631e2a4
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-11-12 21:11:38 +0100

    Documentation bd.class.php et utilisateur.class.php

--- modeles/bd.class.php
+++ modeles/bd.class.php
@@ -1,21 +1,43 @@
 <?php
+/**
+ * @author Félix Autant
+ * @describe classe de la base de données
+ * @version 0.1
+ * @todo faire methode __clone
+ */
 
 class Bd{
+    /**
+     *
+     * @var Bd|null instance de la base de données
+     */
     private static ?Bd $instance = null;
-
+    /**
+     *
+     * @var PDO|null notre pdo
+     */
     private ?PDO $pdo;
 
-    private function __construct(){
+    /**
+     * Notre constructeur par défaut
+     * 
+     * @throws PDOException si la connexion à la base de données échoue
+     */
+    private function __construct() {
         try {
             $this->pdo = new PDO('mysql:host='. DB_HOST . ';dbname='. DB_NAME, DB_USER, DB_PASS);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch(PDOException $e){
-    
             die('Connexion à la base de données échouée : ' . $e->getMessage());
         }
     }
 
+    /**
+     * Get l'instance de la base de données
+     *
+     * @return Bd instance de la base de données
+     */
     public static function getInstance(): Bd{
         if (self::$instance == null){
             self::$instance = new Bd();
@@ -23,16 +45,28 @@ class Bd{
         return self::$instance;
     }
 
+    /**
+     * Get la connexion
+     *
+     * @return PDO notre connexion au pdo
+     */
     public function getConnexion(): PDO{
         return $this->pdo;
     }
 
-    //empecherd de cloner l'objet
+    /**
+     * Empeche de cloner l'objet
+     *
+     * @return void
+     */
     private function __clone(){
-
     }
 
-    //empecher de deserialiser l'objet
+    /**
+     * Empecher la dématérialisation
+     * 
+     * @throws Exception si on tente de déserialiser un singleton
+     */
     public function __wakeup(){
             throw new Exception("Un singleton ne doit pas être deserialisé");
     }
