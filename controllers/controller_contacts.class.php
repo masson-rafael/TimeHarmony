133dfce1db285de45706f2a7d209e698a9245d58
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2024-12-16 10:47:52 +0100

    affichage des photos de profil dans contacts.html.twig, petite modification des chemins pdp en bd

--- controllers/controller_contacts.class.php
+++ controllers/controller_contacts.class.php
@@ -23,9 +23,44 @@ class ControllerContacts extends Controller
         parent::__construct($twig, $loader);
     }
 
+    function recupererContacts($idUtilisateur): array {
+        $pdo = $this->getPdo();
+
+        $managerUtilisateur = new UtilisateurDao($pdo);
+        $contactsId = $managerUtilisateur->findAllContact($idUtilisateur);  
+        $contacts=array();     
+        foreach ($contactsId as $contact) {
+            $contacts[] = $managerUtilisateur->find($contact['idUtilisateur2']);
+        }
+        return $contacts;
+    }
+
+    /**
+     * Fonction appellee par la corbeille pour supprimer un contact
+     *
+     * @return void
+     */
+    public function supprimerContact() {
+        // Récupération de l'id envoyé en parametre du lien
+        $id = $_GET['id'];
+        $type = $_GET['type'];
+        $pdo = $this->getPdo();
+        $manager = new UtilisateurDao($pdo);
+        $manager->supprimerUtilisateur($id);
+        if($type == 'admin') {
+            $this->lister();
+        } else {
+            $this->deconnecter();
+        }
+    }
+
     function lister() {
+        $contacts = $this->recupererContacts($_SESSION['utilisateur']->getId());
         //Génération de la vue
         $template = $this->getTwig()->load('contacts.html.twig');
-        echo $template->render(array());
+        echo $template->render(array(
+            'menu' => 'contacts',
+            'contacts' => $contacts
+        ));
     }
 }
