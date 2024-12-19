f1d028f04e7cccec6fe26de7e4e300655ab36710
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2024-12-19 09:05:23 +0100

    Ajout d'un twig pour les informations diverses et d'un controller pour ces mêmes informations

---
+++ controllers/controller_informations.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @author Thibault Latxague
+ * @describe Controller des informations du site (A propos, Conditions générales d'utilisation et Contact)
+ * @version 0.1
+ */
+
+/**
+ * Undocumented class
+ */
+class ControllerInformations extends Controller {
+    
+    /**
+     * Fonction qui affiche les conditions générales d'utilisation
+     */
+    public function afficherCGDU() {
+
+    }
+}
