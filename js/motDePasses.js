c3bd9dfb30cce8a5bd13ba605a371b7f863de6ab
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2025-01-13 15:19:27 +0100

    Ajout disable bouton inscription si mdp correspondent pas ET/OU respectent pas pregmatch

--- js/motDePasses.js
+++ js/motDePasses.js
@@ -1,4 +1,5 @@
 let formulaire = document.getElementById('form');
+let btn = document.getElementById('boutonInscription');
 
 let mdp = formulaire[3];
 let mdpConfirme = formulaire[4];
@@ -8,13 +9,35 @@ console.log(mdpConfirme);
 
 mdp.addEventListener('input', verifierCorrespondance);
 mdpConfirme.addEventListener('input', verifierCorrespondance);
+mdp.addEventListener('input', () => verifierPattern(mdp));
+mdpConfirme.addEventListener('input', () => verifierPattern(mdpConfirme));
+btn.disabled = true;
 
 function verifierCorrespondance() {
     if (mdp.value !== mdpConfirme.value) {
         mdpConfirme.setCustomValidity('Les mots de passe ne correspondent pas');
         mdpConfirme.style.borderColor = 'red'; // Change la couleur du bord en rouge
+        btn.disabled = true;
     } else {
         mdpConfirme.setCustomValidity('');
         mdpConfirme.style.borderColor = ''; // Réinitialise la couleur par défaut
+        btn.disabled = false;
+    }
+}
+
+function verifierPattern(motDePasse) {
+    const motif = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)[a-zA-Z\d\W]{8,25}$/;
+
+    if (motDePasse.value.length < 8 || motDePasse.value.length > 25 || !motif.test(motDePasse.value)) {
+        motDePasse.setCustomValidity('Le mot de passe doit contenir entre 8 et 25 caractères et au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial');
+        motDePasse.style.borderColor = 'red';
+        btn.disabled = true;
+    } else {
+        motDePasse.setCustomValidity('');
+        motDePasse.style.borderColor = '';
+        btn.disabled = false;
+        if(mdp.value !== mdpConfirme.value) {
+            btn.disabled = true;
+        }
     }
 }
