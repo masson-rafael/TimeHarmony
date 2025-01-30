43a18b76867eca49ada39af5b6e449cfbd3a3b7a
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2025-01-30 09:59:13 +0100

    fix du script

--- timeharmony.sql
+++ timeharmony.sql
@@ -149,7 +149,7 @@ CREATE TABLE IF NOT EXISTS `timeharmony_groupe` (
     `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
     `idProprietaire` int NOT NULL,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`idChef`) REFERENCES `timeharmony_utilisateur` (`id`) ON DELETE CASCADE
+    FOREIGN KEY (`idProprietaire`) REFERENCES `timeharmony_utilisateur` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
