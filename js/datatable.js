641bda2e9c90727ba07481237a9cd944d3753246
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2025-01-30 12:24:39 +0100

    perso des tri dt sur admin et contact

--- js/datatable.js
+++ js/datatable.js
@@ -1,12 +1,40 @@
 $(document).ready(function() {
-    $('.table').DataTable({
-        language: {
-            url: 'https://cdn.datatables.net/plug-ins/2.2.1/i18n/fr-FR.json'
-        },
-        "pageLength": 10,
-        "ordering": true,
-        "responsive": true,
-        "autoWidth": true,
-        "scrollX": true, // Permet d'activer le scroll horizontal
+    $('.table').each(function() {
+        let $table = $(this);
+
+        // Config par défaut
+        let options = {
+            language: {
+                url: 'https://cdn.datatables.net/plug-ins/1.13.6/i18n/fr-FR.json'
+            },
+            columnDefs: [],
+            pageLength: 10,
+            ordering: true,
+            responsive: true,
+            autoWidth: true,
+            scrollX: true,
+            order: [[0, 'asc']]
+        };
+
+        if ($table.hasClass('admin')) {
+            options.columnDefs = [
+                // Désactivation du tri sur les colonnes images de profil et actions
+                { targets: [6, 7], orderable: false }]
+        }
+
+        if ($table.hasClass('contact')) {
+            options.columnDefs = [
+                // Désactivation du tri sur les colonnes images de profil et actions
+                { targets: [0, 4], orderable: false }]
+            options.order = [[1, 'asc']]
+        }
+
+        if ($table.hasClass('groupe')) {
+            options.columnDefs = [
+                // Désactivation du tri sur les colonnes actions
+                { targets: [3], orderable: false }]
+        }
+
+        $table.DataTable(options);
     });
 });
