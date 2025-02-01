bb76809cead26dcf0ef93c326bd5509a801a5a91
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2025-02-01 11:38:05 +0100

    Ajout de 1 fichier js par page de recherche

---
+++ js/formulaireRechercheP1.js
@@ -0,0 +1,61 @@
+let groupes = document.querySelectorAll('input[name="groupes[]"]'); // Récupérer tous les groupes
+let tableObligatoire = document.getElementById('tableObligatoire'); // Récupérer la table des contacts obligatoires
+let btn = document.getElementById('boutonLancerRecherche');
+
+btn.disabled = true;
+console.log(tableObligatoire);
+console.log('groupes');
+
+/* 
+Quand l’utilisateur coche ou décoche un groupe, 
+tous les contacts qui sont dans ce groupe sont aussi cochés ou décochés
+*/
+
+console.log(tableObligatoire);
+console.log('groupes');
+
+let groupesCoches = new Set(); // Ensemble des groupes cochés
+
+groupes.forEach(groupe => {
+    groupe.addEventListener('change', function () {
+        const idGroupe = this.value; // ID du groupe
+        const isChecked = this.checked; // État coché ou décoché du groupe
+
+        if (isChecked) {
+            groupesCoches.add(idGroupe);
+        } else {
+            groupesCoches.delete(idGroupe);
+        }
+
+        // Parcourir les membres du groupe modifié
+        if (membres[idGroupe]) {
+            const users = membres[idGroupe];
+
+            users.forEach(idUtilisateur => {
+                const userCheckbox = document.querySelector(`input[name="contacts[]"][value="${idUtilisateur}"]`);
+
+                // Si l'utilisateur est dans la liste de contact affichée
+                if (userCheckbox) {
+                    if (isChecked) {
+                        userCheckbox.checked = true;
+                        userCheckbox.disabled = true;
+                    } else {
+                        // Vérifier si l'utilisateur appartient à un autre groupe coché
+                        let estDansUnAutreGroupe = false;
+
+                        groupesCoches.forEach(groupeId => {
+                            if (membres[groupeId] && membres[groupeId].includes(idUtilisateur)) {
+                                estDansUnAutreGroupe = true;
+                            }
+                        });
+
+                        if (!estDansUnAutreGroupe) {
+                            userCheckbox.checked = false;
+                            userCheckbox.disabled = false;
+                        }
+                    }
+                }
+            });
+        }
+    });
+});
